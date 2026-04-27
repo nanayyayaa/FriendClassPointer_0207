9701c4bc413792f7553f948e07a85b3a7bb5e103
nanayyayaa <jiejueejuju@gmail.com>
2026-04-27 20:43:51 +0700

    commit4

--- praktikum9.cpp
+++ praktikum9.cpp
@@ -13,3 +13,8 @@ private:
 
 public:
     void input() {
+        cout << "Masukkan diagonal 1 Layang-Layang: ";
+        cin >> d1;
+        cout << "Masukkan diagonal 2 Layang-Layang: ";
+        cin >> d2;
+        cout << "Masukkan sisi 1 Layang-Layang: ";
